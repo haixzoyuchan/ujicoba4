0f24c603b64481f097b110838946b95761365579
haixzoyuchan <wildarahman14.com>
2022-03-09 19:05:02 +0700

    commit keduda

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="/css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaravelWilda</title>
+    <!doctype html>
+    <!DOCTYPE html>
+   
+</head>
+<body>
+    
+    <div class="container">
+        <h2>Selamat Datang</h2>
+        <form action="">
+        <input type="email" placeholder="Email" required>
+        <input type="text" placeholder="Nama" required>
+        <input type="text" placeholder="Tujuan" required>
+        <input type="date" placeholder="Tanggal keberangkatan" required>
+        <input type="date" placeholder="Tanggal kembali" required>
+           <div class="btns">
+            <button type="submit">Pesan</button>
+            <a href="./registration.html"><button</a>
+           </div>
+            
+        </form>
+    </div>
+</body>
+</html>
